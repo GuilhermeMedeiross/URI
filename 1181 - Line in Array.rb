621206c520eca93382256b95c122080fde50f8c2
GuilhermeMedeiross <guilherme.lucas2807@gmail.com>
2020-08-07 12:54:52 -0300

    1181 - done (Ruby)

---
+++ 1181 - Line in Array.rb
@@ -0,0 +1,20 @@
+objective = gets.to_i
+operation = gets.to_s.strip
+matrix = Array.new(3) {Array.new(3)}
+total = []
+
+for i in 0..11
+    sum = 0
+    for j in 0..11
+        number = gets.to_f
+        matrix[i][j] = number
+        sum += number
+    end
+    total[i] = sum
+end
+
+if operation == 'M'
+    total[objective] = total[objective].quo 12
+end
+
+puts total[objective]
