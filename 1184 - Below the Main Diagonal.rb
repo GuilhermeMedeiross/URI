115420f40bdc39a47f0a651deeb2a08f9f57d755
GuilhermeMedeiross <guilherme.lucas2807@gmail.com>
2020-08-10 12:37:17 -0300

    1184 - done (Ruby)

---
+++ 1184 - Below the Main Diagonal.rb
@@ -0,0 +1,18 @@
+operation = gets.to_s.strip
+matrix = Array.new(12) {Array.new(12)}
+total = 0.0
+
+for i in 0..11
+    for j in 0..11
+        matrix[i][j] = gets.to_f
+        if i > j
+            total += matrix.dig(i, j)
+        end
+    end
+end
+
+if operation == 'M'
+    total = total.quo 66
+end
+
+puts "#{format("%.1f", total)}"
